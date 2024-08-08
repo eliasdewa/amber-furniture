6b1d5bac8783b6a616ac1bc75b7c909f8e1aed63
Elias Dewa <54449260+eliasdewa14@users.noreply.github.com>
2024-08-08 05:18:43 -0700

    Create pages and routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,19 @@
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import Home from './pages/Home';
+import Product from './pages/Product';
+import About from './pages/About';
+import Contact from './pages/Contact';
+
 const App = () => {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Amber Furniture
-    </h1>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/product" element={<Product />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </Router>
   )
 }
 export default App
