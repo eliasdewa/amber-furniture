4cdd5fe4ee3f7ddd60e8a7deed79b80b46eaa47a
Elias Dewa <eliasdewa3@gmail.com>
2024-09-15 13:39:26 -0700

    Update cart and place-order page

--- frontend/src/pages/Cart.jsx
+++ frontend/src/pages/Cart.jsx
@@ -3,7 +3,7 @@ import { ShopContext } from "../context/ShopContext";
 import Title from "../components/Title";
 import { MdDelete } from "react-icons/md";
 import CartTotal from "../components/CartTotal";
-import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 const Cart = () => {
   // get Product items and cart items from shop context api
@@ -28,11 +28,8 @@ const Cart = () => {
     // console.log(tempData);
   }, [cartItems]);
 
-  const tableData = [
-    { id: 1, name: "John Doe", age: 30 },
-    { id: 2, name: "Jane Smith", age: 25 },
-    // ... more data
-  ];
+  const navigate = useNavigate();
+
   return (
     <div className="pt-14">
       <div className="text-2xl mb-3">
@@ -43,12 +40,13 @@ const Cart = () => {
       <table className="table-auto border-collapse w-full">
         <thead>
           <tr>
-            <th className="border px-4 py-2">Product Image</th>
-            <th className="border px-4 py-2">Name of Product</th>
-            <th className="border px-4 py-2">Price</th>
-            <th className="border px-4 py-2">Size</th>
-            <th className="border px-4 py-2">Quantity</th>
-            <th className="border px-4 py-2">Remove</th>
+            <th className="table-cell">Item</th>
+            <th className="table-cell">Name</th>
+            <th className="table-cell">Unity Price</th>
+            <th className="table-cell">Size</th>
+            <th className="table-cell">Quantity</th>
+            <th className="table-cell">Total Price</th>
+            <th className="table-cell">Remove</th>
           </tr>
         </thead>
         <tbody>
@@ -62,25 +60,25 @@ const Cart = () => {
             }
             return (
               <tr key={index} className="text-center">
-                <td className="border px-4 py-2">
+                <td className="table-cell">
                   {" "}
                   <img
                     src={productData.image}
                     alt=""
-                    className="w-40 mx-auto"
+                    className="w-32 mx-auto"
                   />
                 </td>
                 {/* Item details */}
-                <td className="border px-4 py-2 text-xs sm:text-lg font-medium">
+                <td className="table-cell">
                   {productData.title}
                 </td>
-                <td className="border px-4 py-2 text-xs sm:text-lg font-medium">
+                <td className="table-cell">
                   ${productData.newPrice}
                 </td>
-                <td className="border px-4 py-2 text-xs sm:text-lg font-medium sm:px-3 sm:py-1 bg-slate-50">
+                <td className="table-cell">
                   {item.size}
                 </td>
-                <td className="border px-4 py-2">
+                <td className="table-cell">
                   <input
                     onChange={(e) =>
                       e.target.value === "" || e.target.value === "0"
@@ -97,8 +95,11 @@ const Cart = () => {
                     defaultValue={item.quantity}
                   />
                 </td>
+                <td className="table-cell">
+                  ${productData.newPrice * item.quantity}
+                </td>
                 {/* delete btn */}
-                <td className="border px-4 py-2">
+                <td className="table-cell">
                   <MdDelete
                     onClick={() => updateQuantity(item.id, item.size, 0)}
                     size={30}
@@ -115,11 +116,9 @@ const Cart = () => {
         <div className="w-full sm:w-[450px]">
           <CartTotal />
           <div className="w-full text-end">
-            <Link to="/place-order">
-              <button className="bg-black text-white text-sm my-8 px-8 py-3">
-                PROCEED TO CHECKOUT
-              </button>
-            </Link>
+            <button onClick={() => navigate('/place-order')} className="bg-black text-white text-sm my-8 px-8 py-3 hover:scale-105">
+              PROCEED TO CHECKOUT
+            </button>
           </div>
         </div>
       </div>
--- frontend/src/pages/PlaceOrder.jsx
+++ frontend/src/pages/PlaceOrder.jsx
@@ -1,68 +1,85 @@
-import { useState } from "react"
-import CartTotal from "../components/CartTotal"
-import Title from "../components/Title"
-import { Link } from "react-router-dom";
+import { useState } from "react";
+import CartTotal from "../components/CartTotal";
+import Title from "../components/Title";
+import { useNavigate } from "react-router-dom";
 
 const PlaceOrder = () => {
-  const [payMethod, setPayMethod] = useState('cash');
+  const [payMethod, setPayMethod] = useState("cash");
+
+  const navigate = useNavigate();
   return (
-    <div className="flex flex-col sm:flex-row justify-between gap-4 pt-5 sm:pt-14 min-h-[80vh] mb-1">
+    <div className="flex flex-col sm:flex-row justify-start gap-20 pt-5 sm:pt-14 min-h-[80vh] mb-1">
       {/* Left side */}
-      <div className="flex flex-col gap-4 w-full sm:max-w-[480px]">
-        <div className="text-xl sm:text-2xl my-3">
-          <Title topic={'Delivery Information'} />
+      <div className="flex flex-col gap-4 w-full sm:max-w-[480px] my-6">
+        <div className="text-xl sm:text-2xl">
+          <Title topic={"Delivery Information"} />
         </div>
         <div className="flex gap-3">
-          <input type="text" placeholder="First name" className="border border-gray-300 rounded py-1.5 px-3.5 w-full" />
-          <input type="text" placeholder="Last name" className="border border-gray-300 rounded py-1.5 px-3.5 w-full" />
+          <input
+            type="text"
+            placeholder="First name"
+            className="border border-gray-300 rounded py-1.5 px-3.5 w-full"
+          />
+          <input
+            type="text"
+            placeholder="Last name"
+            className="border border-gray-300 rounded py-1.5 px-3.5 w-full"
+          />
         </div>
-        <input type="email" placeholder="Email address" className="border border-gray-300 rounded py-1.5 px-3.5 w-full" />
-        <input type="text" placeholder="Street" className="border border-gray-300 rounded py-1.5 px-3.5 w-full" />
+        <input
+          type="email"
+          placeholder="Email address"
+          className="border border-gray-300 rounded py-1.5 px-3.5 w-full"
+        />
+        <input
+          type="text"
+          placeholder="Street"
+          className="border border-gray-300 rounded py-1.5 px-3.5 w-full"
+        />
         <div className="flex gap-3">
-          <input type="text" placeholder="City" className="border border-gray-300 rounded py-1.5 px-3.5 w-full" />
-          <input type="text" placeholder="State" className="border border-gray-300 rounded py-1.5 px-3.5 w-full" />
+          <input
+            type="text"
+            placeholder="City"
+            className="border border-gray-300 rounded py-1.5 px-3.5 w-full"
+          />
+          <input
+            type="text"
+            placeholder="State"
+            className="border border-gray-300 rounded py-1.5 px-3.5 w-full"
+          />
         </div>
         <div className="flex gap-3">
-          <input type="number" placeholder="Zip Code" className="border border-gray-300 rounded py-1.5 px-3.5 w-full" />
-          <input type="text" placeholder="Country" className="border border-gray-300 rounded py-1.5 px-3.5 w-full" />
+          <input
+            type="number"
+            placeholder="Zip Code"
+            className="border border-gray-300 rounded py-1.5 px-3.5 w-full"
+          />
+          <input
+            type="text"
+            placeholder="Country"
+            className="border border-gray-300 rounded py-1.5 px-3.5 w-full"
+          />
         </div>
-        <input type="number" placeholder="Phone" className="border border-gray-300 rounded py-1.5 px-3.5 w-full" />
+        <input
+          type="number"
+          placeholder="Phone"
+          className="border border-gray-300 rounded py-1.5 px-3.5 w-full"
+        />
       </div>
-      {/* Right side */}
-      <div className="mt-8">
-        <div className="mt-8 min-w-80">
-          <CartTotal />
-        </div>
-        <div className="mt-12">
-          <Title topic={'Payment Method'}/>
-          {/* Payment method selection */}
-          <div className="flex gap-3 flex-col">
-            <div className="flex items-center gap-3 border py-2 px-3 cursor-pointer" onClick={() => setPayMethod('telebirr')}>
-              <p className={`min-w-3.5 h-3.5 border rounded-full ${payMethod === 'telebirr' ? 'bg-green' : ''}`}></p>
-              <p className="">telebirr</p>
-            </div>
-            <div className="flex items-center gap-3 border py-2 px-3 cursor-pointer" onClick={() => setPayMethod('CBEbirr')}>
-              <p className={`min-w-3.5 h-3.5 border rounded-full ${payMethod === 'CBEbirr' ? 'bg-green' : ''}`}></p>
-              <p className="">CBEbirr</p>
-            </div>
-            <div className="flex items-center gap-3 border py-2 px-3 cursor-pointer" onClick={() => setPayMethod('amole')}>
-              <p className={`min-w-3.5 h-3.5 border rounded-full ${payMethod === 'amole' ? 'bg-green' : ''}`}></p>
-              <p className="">Amole</p>
-            </div>
-            <div className="flex items-center gap-3 border py-2 px-3 cursor-pointer" onClick={() => setPayMethod('cash')}>
-              <p className={`min-w-3.5 h-3.5 border rounded-full ${payMethod === 'cash' ? 'bg-green' : ''}`}></p>
-              <p className="">Cash On Delivery</p>
-            </div>
-          </div>
-        </div>
+      {/* Right Side */}
+      <div className="my-6 w-full sm:max-w-[480px]">
+        <CartTotal />
         {/* Order btn */}
         <div className="w-full text-end mt-8">
-          <Link to='/orders'>
-            <button className="bg-black text-white text-sm px-16 py-3">Place Order</button>
-          </Link>
+          <button
+            onClick={() => navigate("/orders")}
+            className="bg-black text-white text-sm my-8 px-8 py-3 hover:scale-105"
+          >
+            PROCEED TO PAYMENT
+          </button>
         </div>
       </div>
     </div>
-  )
-}
-export default PlaceOrder
+  );
+};
+export default PlaceOrder;
